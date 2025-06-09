57302403fde56940037fec502c55c0061c0acd92
wxSong <144604943+wenxiang-Song@users.noreply.github.com>
2025-06-09 15:25:54 +0800

    Add files via upload

---
+++ NIRFluor-opt and web/run.py
@@ -0,0 +1,17 @@
+# from processing import process
+# import pandas as pd
+
+# smiles = "CCN(CC)C1=CC=C(C(C2=CC=CC=C2C(O)=O)=C(C(O3)=C/4)C=CC4=[N+](CC)\CC)C3=C1"
+
+# df = pd.DataFrame({"smiles": [smiles]})
+# df.to_csv("./input/target_m.csv", index=False)
+
+# def main():
+#     # 设置超参数
+#     similarity_value = 0.1
+#     process(similarity_value)
+    
+#     print("\n所有分子处理完成！")
+
+# if __name__ == "__main__":
+#     main()
